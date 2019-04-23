ee0c8fc60df668cb45ed4ad9f30c5400c750d633
zlongbin <2569050087@qq.com>
2019-04-23 11:18:55 +0000

    选择图片

--- resources/views/weixin/jssdk.blade.php
+++ resources/views/weixin/jssdk.blade.php
@@ -7,12 +7,11 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <button id="img">选择图片</button>
 </body>
 </html>
 <script src="http://res2.wx.qq.com/open/js/jweixin-1.4.0.js "></script>
 <script>
-    <button id="img">选择图片</button>
     wx.config({
         debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: $js_config['appId'], // 必填，公众号的唯一标识
