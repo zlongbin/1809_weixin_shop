5aa2277456581a970b9457dacb1b6d6abdfab42e
zlongbin <2569050087@qq.com>
2019-04-24 12:08:30 +0000

    存入数据库

---
+++ app/Model/WebUserModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WebUserModel extends Model
+{
+    //
+    protected $table="web_user";
+    public $timestamps = false;
+}
