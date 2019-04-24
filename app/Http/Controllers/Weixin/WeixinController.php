72070541221ed46e37bd0e47282ce40b946cc790
zlongbin <2569050087@qq.com>
2019-04-24 02:52:33 +0000

    xml

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -30,37 +30,29 @@ class WeixinController extends Controller
         $openid = $xml_obj->FromUserName;          //用户OpenID
         $msg_type = $xml_obj->MsgType;             // 消息类型
         if($msg_type=="text"){
-            if($xml_obj->Content=="最新商品"){
+            if(strpos($xml_obj->Content,"最新商品")!==false){
                 $goodsInfo = GoodsModel::orderBy('id','desc')->first();
                 // echo "<pre>";print_r($goodsInfo);echo "</pre>";
                 // echo $goodsInfo['id'];
-                $PicUrl = "/images/QQ图片20190107153840.jpg";
-                $Url = "/goods/detail?goods_id=".$goodsInfo['id'];
+                $PicUrl = "http://1809zhoubinbin.comcto.com/images/QQ图片20190107153840.jpg";
+                $Url = "http://1809zhoubinbin.comcto.com/goods/detail?goods_id=".$goodsInfo['id'];
                 $response = '<xml>
                 <ToUserName><![CDATA['.$wx_id.']]></ToUserName>
                 <FromUserName><![CDATA['.$openid.']]></FromUserName>
                 <CreateTime>'.time().'</CreateTime>
-                <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA["你好"]]></Content>
-              </xml>';
-                // $response = '<xml>
-                // <ToUserName><![CDATA['.$wx_id.']]></ToUserName>
-                // <FromUserName><![CDATA['.$openid.']]></FromUserName>
-                // <CreateTime>'.time().'</CreateTime>
-                // <MsgType><![CDATA[news]]></MsgType>
-                // <ArticleCount>1</ArticleCount>
-                // <Articles>
-                //     <item>
-                //     <Title><![CDATA['.$goodsInfo['name'].']]></Title>
-                //     <Description><![CDATA[description1]]></Description>
-                //     <PicUrl><![CDATA['.$PicUrl.']]></PicUrl>
-                //     <Url><![CDATA['.$Url.']]></Url>
-                //     </item>
-                // </Articles>
-                // </xml>';
+                <MsgType><![CDATA[news]]></MsgType>
+                <ArticleCount>1</ArticleCount>
+                <Articles>
+                    <item>
+                    <Title><![CDATA['.$goodsInfo['name'].']]></Title>
+                    <Description><![CDATA[description1]]></Description>
+                    <PicUrl><![CDATA['.$PicUrl.']]></PicUrl>
+                    <Url><![CDATA['.$Url.']]></Url>
+                    </item>
+                </Articles>
+                </xml>';
             }
-            return $response;
+            echo $response;
         }
     }
 }
-
