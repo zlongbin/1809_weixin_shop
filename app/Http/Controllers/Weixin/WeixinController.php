5ce17e55efcd2fd1f8af8f3a2811c00279326e0f
zlongbin <2569050087@qq.com>
2019-04-24 03:55:43 +0000

    xml

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -62,7 +62,37 @@ class WeixinController extends Controller
                 </xml>';
             }
         }elseif($msg_type=='text'){
-            if(strpos($xml_obj->Content,"最新商品")!==false){
+            if(strpos($xml_obj->Content,"+天气")){
+                $city=explode('+',$xml_obj->Content)[0];
+                // echo "City : ".$city;
+                $url = "https://free-api.heweather.net/s6/weather/now?key=HE1904161042411866&location=".$city;
+                $arr = json_decode(file_get_contents($url),true);
+                // echo '<pre>';print_r($arr);echo "</pre>";               
+                if($arr['HeWeather6'][0]['status']=='ok'){
+                    $fl = $arr['HeWeather6'][0]['now']['fl'];               //摄氏度
+                    $wind_dir = $arr['HeWeather6'][0]['now']['wind_dir'];   //风向
+                    $wind_sc = $arr['HeWeather6'][0]['now']['wind_sc'];     //风力
+                    $hum = $arr['HeWeather6'][0]['now']['hum'];             //湿度
+                    $str="城市 : $city \n"."摄氏度 : $fl \n"."风向 : $wind_dir \n"."风力 : $wind_sc \n"."湿度 : $hum \n";
+    
+                    $response_xml='<xml>
+                    <ToUserName><![CDATA['.$openid.']]></ToUserName>
+                    <FromUserName><![CDATA['.$wx_id.']]></FromUserName>
+                    <CreateTime>'.time().'</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA['.$str.']]></Content>
+                    </xml>';
+                }else{
+                    $response_xml='<xml>
+                    <ToUserName><![CDATA['.$openid.']]></ToUserName>
+                    <FromUserName><![CDATA['.$wx_id.']]></FromUserName>
+                    <CreateTime>'.time().'</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA["城市名不正确"]]></Content>
+                    </xml>';
+                }
+                return $response_xml;
+            }elseif(strpos($xml_obj->Content,"最新商品")!==false){
                 $goodsInfo = GoodsModel::orderBy('id','desc')->first();
                 // echo "<pre>";print_r($goodsInfo);echo "</pre>";
                 // echo $goodsInfo['id'];
@@ -90,8 +120,8 @@ class WeixinController extends Controller
                     </item>
                 </Articles>
                 </xml>';
+                echo $response;
             }
-            echo $response;
         }
     }
     // 获取微信用户信息
