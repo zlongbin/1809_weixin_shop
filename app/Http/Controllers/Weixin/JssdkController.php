cec5e1ae470bf61d849b88d15629c376661036c1
zlongbin <2569050087@qq.com>
2019-04-24 00:31:58 +0000

    测试

--- app/Http/Controllers/Weixin/JssdkController.php
+++ app/Http/Controllers/Weixin/JssdkController.php
@@ -30,11 +30,15 @@ class JssdkController extends Controller
     }
     public function getImg(){
         // echo "<pre>";print_r($_GET);echo "</pre>";
-        $media_id = $_GET['media_id'];
+        // $media_id = $_GET['media_id'];
+        $media_id = "dZla2NK2BY6bmZ4Z4tNNklQiZOORiiQuXKE76DaTn3k6eP3D394mThknSoBwQ6o5";
         $access_token = getAccessToken();
         $url = "http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=" . $access_token . "&media_id=" . $media_id;
         $response = json_decode(file_get_contents($url),true);
         echo "<pre>";print_r($response);echo "</pre>";die;
+        if(!file_exists('/images/uploads')){
+
+        }
     }
     public function imgs(){
 
