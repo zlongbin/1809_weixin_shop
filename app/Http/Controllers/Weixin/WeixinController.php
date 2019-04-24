4da1000c1d8db30052a99df36da1f331bbf33369
zlongbin <2569050087@qq.com>
2019-04-24 01:58:31 +0000

    xml

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -40,17 +40,24 @@ class WeixinController extends Controller
                 <ToUserName><![CDATA['.$wx_id.']]></ToUserName>
                 <FromUserName><![CDATA['.$openid.']]></FromUserName>
                 <CreateTime>'.time().'</CreateTime>
-                <MsgType><![CDATA[news]]></MsgType>
-                <ArticleCount>1</ArticleCount>
-                <Articles>
-                    <item>
-                    <Title><![CDATA['.$goodsInfo['name'].']]></Title>
-                    <Description><![CDATA[description1]]></Description>
-                    <PicUrl><![CDATA['.$PicUrl.']]></PicUrl>
-                    <Url><![CDATA['.$Url.']]></Url>
-                    </item>
-                </Articles>
-                </xml>';
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[你好]]></Content>
+              </xml>';
+                // $response = '<xml>
+                // <ToUserName><![CDATA['.$wx_id.']]></ToUserName>
+                // <FromUserName><![CDATA['.$openid.']]></FromUserName>
+                // <CreateTime>'.time().'</CreateTime>
+                // <MsgType><![CDATA[news]]></MsgType>
+                // <ArticleCount>1</ArticleCount>
+                // <Articles>
+                //     <item>
+                //     <Title><![CDATA['.$goodsInfo['name'].']]></Title>
+                //     <Description><![CDATA[description1]]></Description>
+                //     <PicUrl><![CDATA['.$PicUrl.']]></PicUrl>
+                //     <Url><![CDATA['.$Url.']]></Url>
+                //     </item>
+                // </Articles>
+                // </xml>';
             }
             return $response;
         }
