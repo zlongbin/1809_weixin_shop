be304ee880ce55f1c75fb1471880bf09e8f28800
zlongbin <2569050087@qq.com>
2019-04-24 01:40:59 +0000

    xml

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Weixin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\GoodsModel;
 
 class WeixinController extends Controller
 {
@@ -11,4 +12,48 @@ class WeixinController extends Controller
     public function valid(){
         echo $_GET['echostr'];
     }
+    public function wxEvent(){
+        $content = file_get_contents("php://input");
+        $time = date('Y-m-d H:i:s');
+        $str = $time . $content ."\n";
+        file_put_contents("logs/weixin_event.log",$str,FILE_APPEND);
+        $xml_obj = simplexml_load_string($content);
+        // echo 'ToUserName: '. $xml_obj->ToUserName;echo '</br>';        // 公众号ID
+        // echo 'FromUserName: '. $xml_obj->FromUserName;echo '</br>';    // 用户OpenID
+        // echo 'CreateTime: '. $xml_obj->CreateTime;echo '</br>';        // 时间戳
+        // echo 'MsgType: '. $xml_obj->MsgType;echo '</br>';              // 消息类型
+        // echo 'Event: '. $xml_obj->Event;echo '</br>';                  // 事件类型
+        // echo 'EventKey: '. $xml_obj->EventKey;echo '</br>';
+        // echo 'Content: '. $xml_obj->Content;echo '</br>';              //文字内容
+        
+        $wx_id = $xml_obj->ToUserName;             // 公众号ID
+        $openid = $xml_obj->FromUserName;          //用户OpenID
+        $msg_type = $xml_obj->MsgType;             // 消息类型
+        if($msg_type=="text"){
+            if($xml_obj->Content=="最新商品"){
+                $goodsInfo = GoodsModel::orderBy('id','desc')->first();
+                // echo "<pre>";print_r($goodsInfo);echo "</pre>";
+                // echo $goodsInfo['id'];
+                $PicUrl = "/images/QQ图片20190107153840.jpg";
+                $Url = "/goods/detail?goods_id=".$goodsInfo['id'];
+                $response = '<xml>
+                <ToUserName><![CDATA['.$wx_id.']]></ToUserName>
+                <FromUserName><![CDATA['.$openid.']]></FromUserName>
+                <CreateTime>'.time().'</CreateTime>
+                <MsgType><![CDATA[news]]></MsgType>
+                <ArticleCount>1</ArticleCount>
+                <Articles>
+                    <item>
+                    <Title><![CDATA['.$goodsInfo['name'].']]></Title>
+                    <Description><![CDATA[description1]]></Description>
+                    <PicUrl><![CDATA['.$PicUrl.']]></PicUrl>
+                    <Url><![CDATA['.$Url.']]></Url>
+                    </item>
+                </Articles>
+                </xml>';
+            }
+            return $response;
+        }
+    }
 }
+
