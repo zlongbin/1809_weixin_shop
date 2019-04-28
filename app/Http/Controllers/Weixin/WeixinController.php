e0a8b8ccb29f3bbd64a283cbcd0afb8287b6f67e
zlongbin <2569050087@qq.com>
2019-04-28 10:31:02 +0000

    return

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -21,13 +21,13 @@ class WeixinController extends Controller
         $str = $time . $content ."\n";
         file_put_contents("logs/weixin_event.log",$str,FILE_APPEND);
         $xml_obj = simplexml_load_string($content);
-        echo 'ToUserName: '. $xml_obj->ToUserName;echo '</br>';        // 公众号ID
-        echo 'FromUserName: '. $xml_obj->FromUserName;echo '</br>';    // 用户OpenID
-        echo 'CreateTime: '. $xml_obj->CreateTime;echo '</br>';        // 时间戳
-        echo 'MsgType: '. $xml_obj->MsgType;echo '</br>';              // 消息类型
-        echo 'Event: '. $xml_obj->Event;echo '</br>';                  // 事件类型
-        echo 'EventKey: '. $xml_obj->EventKey;echo '</br>';
-        echo 'Content: '. $xml_obj->Content;echo '</br>';              //文字内容
+        // echo 'ToUserName: '. $xml_obj->ToUserName;echo '</br>';        // 公众号ID
+        // echo 'FromUserName: '. $xml_obj->FromUserName;echo '</br>';    // 用户OpenID
+        // echo 'CreateTime: '. $xml_obj->CreateTime;echo '</br>';        // 时间戳
+        // echo 'MsgType: '. $xml_obj->MsgType;echo '</br>';              // 消息类型
+        // echo 'Event: '. $xml_obj->Event;echo '</br>';                  // 事件类型
+        // echo 'EventKey: '. $xml_obj->EventKey;echo '</br>';
+        // echo 'Content: '. $xml_obj->Content;echo '</br>';              //文字内容
         
         $wx_id = $xml_obj->ToUserName;             // 公众号ID
         $openid = $xml_obj->FromUserName;          //用户OpenID
