b82b7703b94e7026c2117f076f1e7b54c4ed0e39
zlongbin <2569050087@qq.com>
2019-04-30 01:21:55 +0000

    token

--- app/Http/Controllers/YuekaoController.php
+++ app/Http/Controllers/YuekaoController.php
@@ -21,6 +21,8 @@ class YuekaoController extends Controller
         echo "MsgType:" .$xml->MsgType;echo "</br>";                //事件类型
         echo "Content:" .$xml->Content;echo "</br>";                //文本消息
 
+        echo $this->access_token();
+
 //         <xml><ToUserName><![CDATA[gh_6a4f70b5eed6]]></ToUserName>^M
 // <FromUserName><![CDATA[od-A-1FwnuCU3XUp3HU6wtIuDw48]]></FromUserName>^M
 // <CreateTime>1555396243</CreateTime>^M
@@ -29,5 +31,22 @@ class YuekaoController extends Controller
 // <MsgId>22267922250759760</MsgId>^M
 // </xml>^M
 
+    }
+    public function access_token(){
+        $key = "access_token";
+        $access_token = Redis::get($key);
+        if($access_token){
+            return $access_token;
+        }else{
+            $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".env('WX_APP_ID')."&secret=".env('WX_APP_SECRET');
+            $response = json_decode(file_get_contents($url),true);
+            if(isset($response['access_token'])){
+                Redis::set($key,$response['access_token']);
+                Redis::expore($key,3600);
+                return $response['access_token'];
+            }else{
+                return false;
+            }
+        }
     }
 }
