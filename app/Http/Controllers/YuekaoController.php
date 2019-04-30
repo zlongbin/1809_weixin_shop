c920a141909a86a5b829651e0b24f96441a32d2d
zlongbin <2569050087@qq.com>
2019-04-30 02:29:53 +0000

    id

--- app/Http/Controllers/YuekaoController.php
+++ app/Http/Controllers/YuekaoController.php
@@ -75,7 +75,7 @@ class YuekaoController extends Controller
     public function id(){
         $id_url = "https://api.weixin.qq.com/cgi-bin/template/api_add_template?access_token=".$this->access_token();
         $client = new Client;
-        $response = $cliect->request('post',$url,['body'=>["template_id_short"=>"TM00015"]]);
+        $response = $client->request('post',$url,['body'=>["template_id_short"=>"TM00015"]]);
         $body = $response->getBody();
         $json = json_decode($json,true);
         echo $json;
